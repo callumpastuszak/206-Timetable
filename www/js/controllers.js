d68ec922bb796f5deb89d97a96fd32dac96f1b44
Callum Pastuszak <callum.pastuszak@gmail.com>
2017-05-16 21:36:01 +1000

    Testing database
    Testing out diffrent options for the database using the working storage solution I have

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('app.controllers', ['ngStorage'])
 
+
 .controller('homepageCtrl', ['$scope' ,'$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $localStorage, $sessionStorage) {
     $scope.$storage = $localStorage;
-
 }])
 
 
@@ -50,7 +50,6 @@ function ($scope, $stateParams) {
 }])
    
 .controller('cycle22PLTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
@@ -84,13 +83,28 @@ function ($scope, $stateParams) {
 .controller('databaseTemplateCtrl', ['$scope', '$stateParams', '$localStorage', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
+
 function ($scope, $stateParams, $localStorage) {
-$scope.$storage = $localStorage;
-$scope.dddisplay = $scope.$storage.message;
-    $scope.ddclick = function(ddinput){
+$scope.$storage = $localStorage.$default({
+    cycleName: []
+});
+
+//$scope.dddisplay = ($scope.$storage.message);
+    $scope.ddclick = function(){
+        var add = {
+            name: $scope.cycleName,
+            age: $scope.groupName,
+        }
+
+      $localStorage.cycleName.push(add);
+
+
+
         $scope.$storage.message = ddinput;
-        $scope.dddisplay = $scope.$storage.message;
+        $scope.$storage.test = ddinput;
+        $scope.dddisplay = $scope.$storage.test;
         console.log($scope);
     }
 
 }])
+
