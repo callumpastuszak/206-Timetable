746d26fa9b8bfe67c29e4ebee96df99dc18f31de
Callum Pastuszak <callum.pastuszak@gmail.com>
2017-05-22 21:57:23 +1000

    Reverted to old version, modal test I've reverted to an older version in which all the database features were working correctly, additionally I am testing a modal to take the input from the user. After this is complete I will complete the database then it will be all done. yay

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('app.controllers', ['ngStorage'])
 
-
 .controller('homepageCtrl', ['$scope' ,'$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $localStorage, $sessionStorage) {
     $scope.$storage = $localStorage;
+
 }])
 
 
@@ -50,6 +50,7 @@ function ($scope, $stateParams) {
 }])
    
 .controller('cycle22PLTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
@@ -83,28 +84,39 @@ function ($scope, $stateParams) {
 .controller('databaseTemplateCtrl', ['$scope', '$stateParams', '$localStorage', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $localStorage, $ionicModal,) {
+$ionicModal.fromTemplateUrl('databaseTemplate.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 
-function ($scope, $stateParams, $localStorage) {
-$scope.$storage = $localStorage.$default({
-    cycleName: []
-});
 
-//$scope.dddisplay = ($scope.$storage.message);
-    $scope.ddclick = function(){
-        var add = {
-            name: $scope.cycleName,
-            age: $scope.groupName,
-        }
-
-      $localStorage.cycleName.push(add);
-
-
-
-        $scope.$storage.message = ddinput;
-        $scope.$storage.test = ddinput;
-        $scope.dddisplay = $scope.$storage.test;
+$scope.$storage = $localStorage;
+$scope.testdisplay = $scope.$storage.message;
+    $scope.testclick = function(testinput){
+        $scope.$storage.message = testinput;
+        $scope.testdisplay = $scope.$storage.message;
         console.log($scope);
     }
 
 }])
-
