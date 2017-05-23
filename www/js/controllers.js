f303a4bf93d7c8754ea2d5b3648f4a1a86be9af4
Callum Pastuszak <callum.pastuszak@gmail.com>
2017-05-23 10:35:30 +1000

    Its happening The modal worked, not edited to suit the rest of the templates yet.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -84,32 +84,7 @@ function ($scope, $stateParams) {
 .controller('databaseTemplateCtrl', ['$scope', '$stateParams', '$localStorage', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $localStorage, $ionicModal,) {
-$ionicModal.fromTemplateUrl('databaseTemplate.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  // Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-    // Execute action
-  });
-
+function ($scope, $stateParams, $localStorage,) {
 
 $scope.$storage = $localStorage;
 $scope.testdisplay = $scope.$storage.message;
@@ -120,3 +95,62 @@ $scope.testdisplay = $scope.$storage.message;
     }
 
 }])
+
+/**
+.controller('hashtagController', ['$scope', function($scope, $ionicModal) {
+    $scope.modalTest = function() {
+
+        $ionicModal.fromTemplateUrl('hashtag-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up',
+            focusFirstInput: true
+        }).then(function(modal) {
+            $scope.modal = modal;
+            $scope.modal.show();
+        }); 
+    };
+
+    $scope.openModal = function() {
+        $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+
+
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+
+    $scope.$on('modal.hidden', function() {
+        // Execute action
+    });
+
+    $scope.$on('modal.removed', function() {
+        // Execute action
+    });
+}])
+
+**/
+
+.controller('hashtagController', function($scope, $ionicModal) {
+  
+  $scope.contacts = [
+    { name: 'Gordon Freeman' },
+    { name: 'Barney Calhoun' },
+    { name: 'Lamarr the Headcrab' },
+  ];
+
+  $ionicModal.fromTemplateUrl('hashtag-modal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  
+  $scope.createContact = function(u) {        
+    $scope.contacts.push({ name: u.firstName + ' ' + u.lastName });
+    $scope.modal.hide();
+  };
+
+});
